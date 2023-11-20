5df991f3283110d76c5434af45bc426ea533b5ca
Tare Ebelo <75279482+TareHimself@users.noreply.github.com>
2023-11-20 10:26:59 -0500

    added initialize

--- src/nodeml_portaudio/exports.cc
+++ src/nodeml_portaudio/exports.cc
@@ -1,9 +1,20 @@
 #include <napi.h>
+#include <portaudio.h>
+
+void cleanup()
+{
+    Pa_Terminate();
+}
 
 Napi::Object InitModule(Napi::Env env, Napi::Object exports)
 {
+    if(Pa_Initialize() != paNoError){
+        Napi::Error::New(env,"Failed To Initialize PortAudio");
+    }
+
+    env.AddCleanupHook(cleanup);
+
     return exports;
 }
 
-
-NODE_API_MODULE(nodeml_gpt4all, InitModule);
+NODE_API_MODULE(nodeml_portaudio, InitModule);
